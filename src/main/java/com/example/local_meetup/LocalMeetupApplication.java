58b0efedea68eb0f274bf229eea3ca39beb3e5dd
ChoiEunHyuk <ehchoi0206@gmail.com>
2025-08-06 10:05:16 +0900

    first commit

---
+++ src/main/java/com/example/local_meetup/LocalMeetupApplication.java
@@ -0,0 +1,13 @@
+package com.example.local_meetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalMeetupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocalMeetupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/local_meetup/LocalMeetupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.local_meetup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalMeetupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
